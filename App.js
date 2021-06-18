c1a0a3583a5b22a521af545b54f95a3f82be5792
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-19 00:55:32 +0600

    22 React Native Functional State
    22 React Native Functional State

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
-import React, {Component} from 'react';
-import {Button, Image, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
 
-class App extends Component {
 
-    constructor() {
-        super();
-        this.state={
-            name:"Sujon",
-            age:"29",
-            city:"dhaka"
-        }
-    }
 
-    change=()=>{
-        this.setState({name:"Liya",age:"18",city:"BD, GB"})
-}
+const App = () => {
 
-    render() {
-        return (
-           <View>
-               <Text>{this.state.name}</Text>
-               <Text>{this.state.city}</Text>
-               <Text>{this.state.age}</Text>
-               <Button onPress={this.change} title="Click Me"/>
-           </View>
-        );
-    }
-}
+    const [state, setState] = useState({
+        name:"Ruhul Amin",
+        city:"Dhaka"
+    });
+
+    return (
+        <View>
+            <Text>{state.name}</Text>
+            <Text>{state.city}</Text>
+        </View>
+    );
+};
 
 export default App;
