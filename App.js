4d30eff7418acc713416380c3167c0a5043d1c81
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-22 02:37:02 +0600

    33 Core Component FlatList
    33 Core Component FlatList

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
 import React, {Component} from 'react';
-import {Alert, Button, Image, ScrollView, View} from 'react-native';
+import {Alert, Button, FlatList, Image, ScrollView, Text, View} from 'react-native';
 import WebView from 'react-native-webview';
 
 class App extends Component {
 
-    alertMe=()=>{
-        Alert.alert("Sujon");
+    MyData=[
+        {title:"Bangladesh",subtitle:"Country Name, Nation name"},
+        {title:"India",subtitle:"Country Name, Nation name"},
+        {title:"Pakistan",subtitle:"Country Name, Nation name"},
+        {title:"Nepal",subtitle:"Country Name, Nation name"},
+        {title:"Bhutan",subtitle:"Country Name, Nation name"},
+        {title:"Australia",subtitle:"Country Name, Nation name"},
+        {title:"USA",subtitle:"Country Name, Nation name"},
+    ];
+
+    ChildView=({Childtitle,Childsubtitle})=>{
+        return(
+            <View style={{backgroundColor:'white',padding:10,margin:10}} >
+                <Text style={{color:'black',fontSize:18}}>{Childtitle}</Text>
+                <Text style={{color:'black',fontSize:12}}>{Childsubtitle}</Text>
+            </View>
+        )
     }
 
     render() {
         return (
-            <ScrollView horizontal={true}>
-                <View style={{backgroundColor: 'red', height:100,width:360}}/>
-                <View style={{backgroundColor: 'green', height:100,width:360}}/>
-                <View style={{backgroundColor: 'blue', height:100,width:360}}/>
-                <View style={{backgroundColor: 'teal', height:100,width:360}}/>
-                <View style={{backgroundColor: 'red', height:100,width:360}}/>
-                <View style={{backgroundColor: 'green', height:100,width:360}}/>
-                <View style={{backgroundColor: 'blue', height:100,width:360}}/>
-                <View style={{backgroundColor: 'teal', height:100,width:360}}/>
-                <View style={{backgroundColor: 'red', height:100,width:360}}/>
-                <View style={{backgroundColor: 'green', height:100,width:360}}/>
-                <View style={{backgroundColor: 'blue', height:100,width:360}}/>
-                <View style={{backgroundColor: 'teal', height:100,width:360}}/>
-            </ScrollView>
+            <FlatList data={this.MyData} renderItem={({item})=><this.ChildView Childtitle={item.title} Childsubtitle={item.subtitle} />}/>
+
         );
     }
 }
