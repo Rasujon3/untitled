b92d19e1293dc565b52fb3745bc30f149b03232a
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-22 00:23:41 +0600

    26 final

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import ComponentOne from './ComponentOne';
+import { View} from 'react-native';
 
 class App extends Component {
     render() {
         return (
             <View style={{flexDirection:'row',height:"100%",width:"100%"}}>
-            <View style={{backgroundColor:'orange',height:"100%",width:"32%"}}>
-            <View style={{backgroundColor:'red',height:"100%",width:"32%"}}>
-            <View style={{backgroundColor:'black',height:"100%",width:"32%"}}>
+            <View style={{backgroundColor:'orange',height:"100%",width:"33%"}}></View>
+            <View style={{backgroundColor:'red',height:"100%",width:"33%"}}></View>
+            <View style={{backgroundColor:'black',height:"100%",width:"34%"}}></View>
 
-            </View>
-            </View>
-            </View>
+
+
+
             </View>
         );
     }
