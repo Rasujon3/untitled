edc23d8f1d3227268ab9d42c3211fc2b978822d9
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-22 14:38:17 +0600

    38 Core Component Flatlist keyExtractor={item => item.id}
    38 Core Component Flatlist
    
    keyExtractor={item => item.id}

--- App.js
+++ App.js
@@ -6,71 +6,85 @@ class App extends Component {
 
     MyData = [
         {
+            id:"1",
             title: 'Bangladesh',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
         },
         {
+            id:"2",
             title: 'India',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/19/06/sunset-6344387_960_720.jpg',
         },
         {
+            id:"3",
             title: 'Pakistan',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
         },
         {
+            id:"4",
             title: 'Nepal',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/19/06/sunset-6344387_960_720.jpg',
         },
         {
+            id:"5",
             title: 'Bhutan',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
         },
         {
+            id:"6",
             title: 'Australia',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/19/06/sunset-6344387_960_720.jpg',
         },
         {
+            id:"7",
             title: 'USA',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
         },
         {
+            id:"8",
             title: 'Bangladesh',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
         },
         {
+            id:"9",
             title: 'India',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/19/06/sunset-6344387_960_720.jpg',
         },
         {
+            id:"10",
             title: 'Pakistan',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
         },
         {
+            id:"11",
             title: 'Nepal',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/19/06/sunset-6344387_960_720.jpg',
         },
         {
+            id:"12",
             title: 'Bhutan',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
         },
         {
+            id:"13",
             title: 'Australia',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/19/06/sunset-6344387_960_720.jpg',
         },
         {
+            id:"14",
             title: 'USA',
             subtitle: 'Country Name, Nation name',
             img: 'https://cdn.pixabay.com/photo/2021/06/17/04/42/man-6342665_960_720.jpg',
@@ -103,7 +117,7 @@ class App extends Component {
 
     render() {
         return (
-            <FlatList numColumns={2} horizontal={false} data={this.MyData}
+            <FlatList keyExtractor={item => item.id} numColumns={2} horizontal={false} data={this.MyData}
                       renderItem={({item}) => <this.ChildView Childtitle={item.title} Childsubtitle={item.subtitle}
                                                               img={item.img}/>}/>
 
