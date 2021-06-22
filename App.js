860b8ec304dd583f574ff29d5a23783e01d0f506
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-22 21:01:21 +0600

    40 Core Component TextInput simple calculator
    40 Core Component TextInput simple calculator

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
-import {Text, TextInput, View} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            num1:0,
+            num2:0,
+            result:" "
+        }
+    }
+    addTwo=()=>{
+        var x = this.state.num1+this.state.num2;
+        this.setState({result:x.toString()})
+    }
+
     render() {
         return (
-            <View>
-                <TextInput multiline={false} autoFocus={true} maxLength={5} textAlign='center' placeholderTextColor='red' placeholder="your Name" style={{margin:20,padding:10,borderWidth:1,borderColor:'black',height:50}}></TextInput>
+            <View style={{padding:20}}>
+                <TextInput onChangeText={(text)=>{this.setState({num1: parseInt(text)})}} placeholder="Number One" style={{marginBottom:10,padding:10,borderWidth:1,borderColor:'black',height:40}}></TextInput>
+                <TextInput onChangeText={(text)=>{this.setState({num2: parseInt(text)})}} placeholder="Number Two" style={{marginBottom:10,padding:10,borderWidth:1,borderColor:'black',height:40}}></TextInput>
+                <Button onPress={this.addTwo} title="Add" />
+                <Text style={{margin:10,fontSize:24}}>Result: {this.state.result}</Text>
             </View>
         );
     }
