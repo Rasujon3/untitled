e98163191723bdbcc4a737c1d4961d58345458f4
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-22 23:55:48 +0600

    42 Core Component Picker
    42 Core Component Picker

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
-import {StatusBar, View} from 'react-native';
+import {Picker, StatusBar, Text, View} from 'react-native';
 
 class App extends Component {
+constructor() {
+    super();
+    this.state={
+        selectedItemValue:" ",
+        selectedItemPosition:" "
+
+    }
+}
     render() {
         return (
             <View>
-                <StatusBar hidden={true} barStyle="dark-content" backgroundColor="#000000"/>
+                <Picker
+                    selectedValue={this.state.selectedItemValue}
+                    onValueChange={(itemValue, itemPosition)=>
+                    {this.setState({selectedItemValue:itemValue,
+                        selectedItemPosition:itemPosition})}}
+                >
+                    <Picker.Item label="Bangladesh" value="Bangladesh"/>
+                    <Picker.Item label="India" value="India"/>
+                    <Picker.Item label="Pakistan" value="Pakistan"/>
+                    <Picker.Item label="Canada" value="Canada"/>
+                    <Picker.Item label="America" value="America"/>
+                </Picker>
+                <Text style={{fontSize: 40}}>{this.state.selectedItemValue}</Text>
             </View>
         );
     }
