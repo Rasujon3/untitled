401df0eab73d9d91a73f7d3c004c49d280463049
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-23 02:21:21 +0600

    52 Core Component Sectionlist "stickySectionHeadersEnabled"
    52 Core Component Sectionlist "stickySectionHeadersEnabled"

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
     itemHeadChild = ({header}) => {
         return (
-            <View style={{height: 20, width: '100%', backgroundColor: 'blue'}}>
+            <View style={{height: 40,padding:10, width: '100%', backgroundColor: 'teal'}}>
                 <Text onPress={this.childHeadClick.bind(this,header)} style={{color:'white'}}>{header}</Text>
             </View>
         );
@@ -29,18 +29,28 @@ class App extends Component {
     DATA = [
         {title: 'Bangladesh', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
         {title: 'India', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'Canada', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'America', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'England', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'Kania', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
         {title: 'Pakistan', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'Peru', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'Bangladesh', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'India', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'Canada', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'America', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'England', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'Kania', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
         {title: 'Pakistan', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
-        {title: 'Pakistan', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
-        {title: 'Pakistan', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
-        {title: 'Pakistan', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
-        {title: 'Pakistan', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']},
+        {title: 'Peru', data: ['Rangpur', 'Dhaka', 'Khulna', 'Rajshahi']}
     ]
 
     render() {
         return (
             <SafeAreaView>
                 <SectionList
+                    // inverted={-1}
+                    stickySectionHeadersEnabled={true}
                     sections={this.DATA}
                     renderItem={({item}) => <this.itemChild itemData={item}/>}
                     renderSectionHeader={({section:{title}})=><this.itemHeadChild header={title}/>}
